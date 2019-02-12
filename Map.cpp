1c8be7508a61c10b9b7d06a95e2c7087cf24b5ab
Simon Huang <simon@simonhuang.ca>
2019-02-12 01:31:10 -0500

    Checkpoint: Using Vector instead of Unordered_set. Working on getNode()

--- Map.cpp
+++ Map.cpp
@@ -1,46 +1,21 @@
-/*
-Very likely that I'll rewrite the entire code to be more simple.
-I feel like the things I'm using are out of the scope of what we are supposed to learn.
-Lots of Googling involved...
-*/
-
 #include "Map.h"
 #include <iostream>
 #include <string>
-#include <unordered_set> //.find: avg O(1), worst O(n)
+#include <vector>
 
-template <typename T> struct Node;
-template <typename T> struct Edge;
-
-//Custom Hash and Comparison for unordered_set
-//We need that since we are passing user defined objects
-namespace std {
-	//Custom hash for Node
-	template<typename T> struct hash<Node<T>> {
-		std::size_t operator()(const Node<T> &obj) const {
-			return std::hash<int>()(obj.content);
-		}
-	};
-
-	//Custom hash for Edge
-	template<typename T> struct hash<Edge<T>> {
-		std::size_t operator()(const Edge<T> &obj) const {
-			return std::hash<int>()(obj.to);
-		}
-	};
-
-
-};
-
+template<typename T> struct Node;
+template<typename T> struct Edge;
 
 template<typename T> struct Node {
 	T content;
-	std::unordered_set<Edge<T>> edgeList;
+	std::vector<Edge<T>> edgeList;
 
-	Node(T content) : content(content) {};
+	Node(T content) : 
+		content(content) 
+	{};
 
 	void addEdge(Edge<T> edge) {
-		edgeList.insert(edge);
+		edgeList.push_back(edge);
 	}
 };
 
@@ -48,33 +23,47 @@ template<typename T> struct Edge {
 	Node<T> to;
 	int weight;
 
-	Edge(Node<T> to, int weight) : to(to), weight(weight) {};
+	Edge(Node<T> &to, int weight) : 
+		to(to), weight(weight) 
+	{};
 };
 
 template<typename T> class Map {
 public:
-	Map() {};
+	Map() 
+	{};
 
 	void addNode(T content) {
 		Node<T> node(content);
-		nodeList.insert(node);
+		nodeList.push_back(node);
 	};
 
+	Node<T> getNode(T content) {
+		auto it = std::find(nodeList.begin(), nodeList.end(), content);
+		if (it != nodeList.end()) {
+			return it->content;
+		} else {
+			return NULL;
+		}
+	}
+	
 	void displayAllNodes() {
-		for (auto iterator = nodeList.begin; iterator != nodeList.end; iterator++) {
-			std::cout << *iterator << std::endl;
+		for (auto it = nodeList.begin(); it != nodeList.end(); ++it) {
+			std::cout << it->content << std::endl;
 		}
 	}
 
 private:
-	std::unordered_set<Node<T>> nodeList;
+	std::vector<Node<T>> nodeList;
 };
 
 int main() {
-	Map<int> map; //Create a map of strings
+	Map<int> map;
 	map.addNode(9);
 	map.addNode(7);
 	map.addNode(0);
 	map.addNode(34);
 	map.addNode(87);
+	map.displayAllNodes();
+	Node<int> find = map.getNode(9);
 }
