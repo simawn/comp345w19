863f3087c9d87b8ac1e2f997a2c7ad3449d0f959
Simon Huang <simon@simonhuang.ca>
2019-02-25 15:40:47 -0500

    Checkpoint: Moved all files related to map generation to /data. Added external library link in project. JSON library seems to be working.

--- GameMapLoader.cpp
+++ GameMapLoader.cpp
@@ -1,21 +1,19 @@
-//#include "lib/nlohmann/json.hpp"
 #include "City.h"
 #include "GameMap.h"
 #include "GameMapLoader.h"
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 void openMap(std::string path) {
-	std::string line;
-	std::ifstream file (path);
-	if (file.is_open) {
-		while (getline(file, line)) {
-			std::cout << line << std::endl;
-		}
-		file.close;
-	} else {
-		std::cout << "Unable to load file" << std::endl;
-	}
+	std::ifstream file(path);
+	json jsonMap;
+	file >> jsonMap; //jsonMap will be 'null' if .json is not valid
+	std::cout << jsonMap << std::endl << std::endl;
+	std::cout << jsonMap["1"]["Boston"] << std::endl; //test
 }
 
 void createMap() {
