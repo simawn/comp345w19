0ab19e174493ca835074dc6096f45c3b5c916fc6
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-02-27 14:17:18 -0500

    start of PowerPlant cards

---
+++ PowerPlantCard.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Resources.h"
+class PowerPlantCard {
+private:
+	int maxCitiesSupplied;
+	int cardNumber;
+	PowerPlantCard();
+};
--- SummaryCard.h
+++ SummaryCard.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <string>; 
 class SummaryCard {
-	
-	private:
-		struct GameSteps {
-			std::string General;
-			std::string Details;
-		};
+
+private:
+	struct GameSteps {
+		std::string General;
+		std::string Details;
+	};
 	GameSteps StepsOfTheGame[5];
+	struct GameSteps one, two, three, four, five;
 	SummaryCard();
 	GameSteps getAStep(int stepNumber);
+	std::string getGeneral(int stepNumber);
+	std:: string getDetails(int stepNumber);
 };
