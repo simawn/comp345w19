ee5b25fd42d278b971b48a2837fb711662a365a4
Pierre-Alexis Barras <37449731+Pyxsys@users.noreply.github.com>
2019-03-02 18:21:29 -0500

    Update Deck.cpp
    Updated to function with the card class via inheritance

--- Deck.cpp
+++ Deck.cpp
@@ -1,75 +1,84 @@
 #include "Deck.h"
-#include <iostream>
-#include <vector>
-#include <ctime>
 
-template <class V> class Deck {
-private:
-	std::vector<V > stack;
-
-public:
-	//constructors
-	Deck() {
-		//default constructor
+	/**Default Deck constructor
+	*/
+	Deck::Deck() {
 	}
 
-	Deck(const V src[], int size) {
-		fill(src, size);
+	/**General Constructor
+	*@param Powerplant[] src
+	*@param int size - size of powerplant array
+	*generates a deck and then fills it with the members of the source array
+	*/
+	Deck::Deck(const Cards src[], int size) {
+		Deck::fill(src, size);
 	}
 
-	//member functions
-
-	void fill(const V src[], int size) {
+	/**Fill
+	*@param Powerplant[] src
+	*@param int size - size of powerplant array
+	*fill a deck with the members of the source array
+	*/
+	void Deck::fill(const Cards src[], int size) {
 		//fill deck with elements
 		for (int i = 0; i < size;i++) { 
-			stack.push_back(src[i]);
+			Deck::stack.push_back(src[i]);
 		}
 	}
 
-	void shuffle() {
+	/**push
+	*@param Cards card
+	*adds 'card' to the top of the deck.
+	*/
+	void Deck::push(const Cards card) {
+		Deck::stack.push_back(card);
+	}
+
+	/**insert
+	*@param Cards card
+	*adds 'card' to the bottom of the deck.
+	*/
+	void Deck::insert(const Cards card) {
+		Deck::stack.insert(card);
+	}
+
+	/**shuffle
+	shuffles the elements of the vector in an inline fashion.
+	*/
+	void Deck::shuffle() {
 		//grabs a random value in the vector and pushes at the back.
 		//the range of this decreases until there are no more 'ordered' values
 
 		int x;
 		srand(time(0));				//allows true random number generation
 
-		for (int i = stack.size(); i > 0; i--) {
+		for (int i = Deck::stack.size(); i > 0; i--) {
 			x = rand() % i;								//selects an index in the range of 0 - i
-			stack.push_back(stack[x]);					//pushes the value at that index to the back;
-			stack.erase(stack.begin() + x);				//removes that index, shifting everything down
+			Deck::stack.push_back(Deck::stack[x]);					//pushes the value at that index to the back;
+			Deck::stack.erase(stack.begin() + x);				//removes that index, shifting everything down
 		}
-		stack.shrink_to_fit();
+		Deck::stack.shrink_to_fit();
 	}
 
-	void print() {	
+	/*print
+	Displays all elements in the vector with the back most element on the right.
+	*/
+	void Deck::print() {	
 		//displays deck
-		cout << "BOTTOM\t";
-		for(const V& D : stack) { std::cout << D << " "; }
+		std::cout << "BOTTOM\t";
+		for(const Cards& D : Deck::stack) {
+			std::cout << D << " "; 
+		}
 		std::cout <<"\tTOP"<< std::endl;
 	}
 	
-	V draw() {
+	/**draw
+	*@returns Card
+	*pops the backmost Powerplant card element form the vector and returns it
+	*/
+	Cards Deck::draw() {
 		// returns the last element of the vector, and removes it 
-		V x = stack.back();
-		stack.pop_back();
+		Cards x = Deck::stack.back();
+		Deck::stack.pop_back();
 		return x;
 	}
-};
-
-/*
-int main() {
-	//example driver using integer array
-	int capacity = 10;
-	const int cardsData[10] = { 0,1,2,3,4,5,6,7,8,9 };
-	Deck<int> myDeck(cardsData,capacity);
-	myDeck.print();
-	myDeck.shuffle();
-	
-	
-	int card = myDeck.draw();
-	std::cout <<"drew card: "<< card << std::endl;
-	myDeck.print();
-
-	return 0;
-}
-*/
