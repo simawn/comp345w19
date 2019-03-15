e4945ab0412bf790591e89db35a09e0971f9ed8c
Max <mpage-sowik@outlook.com>
2019-03-14 21:18:16 -0400

    added some methods to part 1 header file

--- GameSetupDriver.h
+++ GameSetupDriver.h
@@ -2,7 +2,11 @@
 #include <iostream>
 #include "../Player.h"
 //Part1
-
+void loadPowerPlants();
+void loadSummaryCards();
+void loadStep3();
+void selectMap();
+void choosePlayableArea(Player p);
 int getPlayerCount();
 const char * setPlayerColours(int pn);
 void placeResourcesonMap();
