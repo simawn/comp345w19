e00888242cf1c5a6bc68e622597edfd2c4eb4568
Pierre-Alexis Barras <37449731+Pyxsys@users.noreply.github.com>
2019-03-22 10:57:57 -0400

    Create BureaucracyStepDriver.h
    moved to drivers. header for bureaucracy step

---
+++ drivers/BureaucracyStepDriver.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../Marketplace.h"
+#include "../Player.h"
+#include "../Resources.h"
+#include <vector>
+
+//Part 4 Phase 5
+/*Bureaucracy, where the players earn cash, re-supply the resource market, and remove a power plant
+from the power plant market, replacing it with a new one from the stack.*/
+
+extern std::vector<Player> currentPlayers;
+extern int step; 
+//need to implement global access to a Marketplace object
+
+void doBureaucracy(Marketplace x);
+void updateCardMarket(Marketplace* x);
+void payPlayer();
+void resupplyResourceMarket();
+void checkForStep2();	//must be implemented
+void resourceTableSolution(); //consults the resource table and determines howmuch of each resource is going into the resource market.
