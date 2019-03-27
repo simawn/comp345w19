c37d5acdcab6f8c93be287d44d397ec7efb1a92b
Pierre-Alexis Barras <37449731+Pyxsys@users.noreply.github.com>
2019-03-27 10:28:03 -0400

    Add files via upload
    bureaucracy now working

--- Tester.cpp
+++ Tester.cpp
@@ -14,12 +14,6 @@
 using namespace std;
 
 
-//Below are global variables that are used for bureaucracy test--
-int step = 1;	//should be declared in a game driver eventually
-Marketplace market = setupMarketplace();
-vector<Player*> currentPlayers_;
-GameMap map_;
-//--
 
 void testPlayer(){
 	cout << "\n === START TESTING PLAYER === \n";
@@ -92,92 +86,19 @@ void testAuction() {
 }
 
 void testBureaucracy() {
-	cout << "\n === TESTING BUREAUCRACY ===\nCreating a mock game-state...";
-	//generates mock gamestate to test bureaucracy
-	Player* pa = new Player("(pa) RED");
-	Player* pc = new Player("(pc) GREEN");
-	Player* pd = new Player("(pd) YELLOW");
-	currentPlayers_.push_back(pa);
-	currentPlayers_.push_back(pc);
-	currentPlayers_.push_back(pd);
-	//Powerplants
-	Gas* g = new Gas(1);
-	Coal* c = new Coal(1);
-	Garbage* ga = new Garbage(1);
-	Uranium* u = new Uranium(1);
-	Cards* PowerPlantCard3 = new PowerPlant(3, 1, 2, g);
-	Cards* PowerPlantCard4 = new PowerPlant(4, 1, 2, c);
-	Cards* PowerPlantCard5 = new PowerPlant(5, 1, 2, g, c);
-	Cards* PowerPlantCard6 = new PowerPlant(6, 1, 1, ga);
-	Cards* PowerPlantCard7 = new PowerPlant(7, 2, 3, g);
-	Cards* PowerPlantCard8 = new PowerPlant(8, 2, 3, c);
-	Cards* PowerPlantCard9 = new PowerPlant(9, 1, 1, g);
-	Cards* PowerPlantCard10 = new PowerPlant(10, 2, 2, c);
-
-	//Cities
-	City* c1 = new City("Montreal");
-	City* c2 = new City("Toronto");
-	City* c3 = new City("Calgary");
-	City* c4 = new City("Vancouver");
-
-	//Game begins
-	std::cout << "Player (pa) buys PP3 and City 1" << std::endl;
-	//Player a
-	pa->buyPowerPlant((PowerPlant*)PowerPlantCard3);
-	pa->buyCity(c1);
-	pa->buyResources(g);
-	std::cout << "Player (pc) buys City 2" << std::endl;
-	//Player c
-	pc->buyCity(c2);
-
-	std::cout << "Player (pc) buys PowerPlant8" << std::endl;
-	//Same amount of city but different powerplants
-	pc->buyPowerPlant((PowerPlant*)PowerPlantCard8);
-	pc->buyPowerPlant((PowerPlant*)PowerPlantCard6);
-	pc->buyResources(c);
-	pc->buyResources(c);
-	std::cout << "Player (pd) buys City 3 and City 4" << std::endl;
-	//Player d
-	pd->buyCity(c3);
-	pd->buyCity(c4);
-	pd->buyPowerPlant((PowerPlant*)PowerPlantCard5);
-	pd->buyResources(c);
-	pd->buyResources(g);
-	pd->buyResources(g);
-
-	market.print();
-	//end of mock setup
-
-	cout << "\n === BEGIN BUREAUCRACY===\n";
-	doBureaucracy();
+	cout << "\n === TESTING BUREAUCRACY ===\n";
+//	doBureaucracy();
 	cout << "\n === out of bureaucracy===\n";
 	market.print();
 	cout << "\n === END TESTING BUREAUCRACY ===\n";
-
-	//Deletes mock state
-	currentPlayers_.pop_back();
-	currentPlayers_.pop_back();
-	currentPlayers_.pop_back();
-	delete pa;
-	delete pc;
-	delete pd;
-	delete PowerPlantCard3;
-	delete PowerPlantCard4;
-	delete PowerPlantCard5;
-	delete PowerPlantCard6;
-	delete PowerPlantCard7;
-	delete PowerPlantCard8;
-	delete PowerPlantCard9;
-	delete PowerPlantCard10;
-	delete c1;
-	delete c2;
-	delete c3;
-	delete c4;
 }
 
 void testResourceBuying() {
 	Phase3();
 }
+
+int step = 1; //should be moved into a game driver eventually.
+Marketplace market = setupMarketplace(); //used for bureaucracy test
 
 int main() {
 
@@ -194,7 +115,7 @@ int main() {
 	//testResourceBuying();
 
 	/*Part 4: Game play: main game loop Phase 5*/
-	testBureaucracy();
+	//testBureaucracy();
 
 	/*Other tests*/
 	//testMarketplaceCreation();
