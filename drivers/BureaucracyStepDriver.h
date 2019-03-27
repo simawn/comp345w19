c53a6443d362ba12f4d089bc952d874f22470b39
Pierre-Alexis Barras <37449731+Pyxsys@users.noreply.github.com>
2019-03-27 10:31:43 -0400

    Update BureaucracyStepDriver.h
    updated bureacracy

--- drivers/BureaucracyStepDriver.h
+++ drivers/BureaucracyStepDriver.h
@@ -2,6 +2,7 @@
 #include "../Marketplace.h"
 #include "../Player.h"
 #include "../Resources.h"
+#include "../GameMap.h"
 #include <vector>
 
 //Part 4 Phase 5
@@ -12,6 +13,7 @@ from the power plant market, replacing it with a new one from the stack.*/
 extern int step; 
 extern Marketplace market;
 extern std::vector<Player*> currentPlayers_;//value should be refactored in final build to whatever the global player vector will be
+extern GameMap map_;//value should be refactored in final build to whatever the global map will be
 
 /*performs the bureaucracy steps*/
 void doBureaucracy();
@@ -23,7 +25,8 @@ void payPlayers();
 void resupplyResourceMarket();
 /**consults the resource table and determines how much of each resource is going into the resource market*/
 void resourceTableSolution();
-
+/**resupplies the resource market with the amount of the resource type*/
+void resupplyOfType(int amount, std::string type);
 /**returns the amount of electro earned for powering an amount of cities*/
 int getPoweredCityValue(int);
 /*checks if integer entry already exists within the integer vector*/
@@ -32,3 +35,9 @@ bool isNewEntry(const std::vector<int>, const int);
 void powerPlants(PowerPlant * PP, Player * p, int Total, Resource * type);
 /*remove the appropriate amount of resurces from a player to their partitioning of two types*/
 void powerPlants(PowerPlant * PP, Player * p, Resource * type1, Resource * type2);
+
+/**used for testing the filling of resource market*/
+void setupTestResourceMap();
+
+/**returns the smaller integer value of the two provided integers*/
+int min(const int a, const int b);
