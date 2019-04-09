5bd81d1db34526691fbc558b3b6ba6116ce326da
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-04-08 20:36:43 -0400

    fix non compiling error for decorator

--- CoalPowerPlant.h
+++ CoalPowerPlant.h
@@ -5,4 +5,12 @@ class CoalPowerPlant :public CardsDecorator {
 public:
 	CoalPowerPlant(Cards * decoratedCards);
 	void print();
+	/**Get the cost of the given powerplant.*/
+	int getCost();
+	/**Get the amount of the given resource it costs.*/
+	int getResourceCost();
+	/**Get the housing amount that it powers.*/
+	int getHouseValue();
+	/**Get the resource type for the powerplant ie Coal.*/
+	std::vector<Resource *> getResourceType();
 };
