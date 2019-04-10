733098bd7ef4578b788f743ff16d448292ee8cee
Max <mpage-sowik@outlook.com>
2019-04-10 13:42:06 -0400

    adding environmetal strategy

--- PlayerBotEnvironmentalist.h
+++ PlayerBotEnvironmentalist.h
@@ -2,6 +2,9 @@
 #include "PlayerBot.h"
 class PlayerBotEnvironmentalist : public PlayerBot {
 public:
+	/**Only buy powerplants that are renewable otherwise don't buy*/
+	int auctionDecision(Marketplace* marketplace) override;
+	int bidDecision(Cards* card, int currentBid) override;
 	PlayerBotEnvironmentalist() : Player() {};
 	PlayerBotEnvironmentalist(std::string colour) : Player(colour) {};
 	~PlayerBotEnvironmentalist();
